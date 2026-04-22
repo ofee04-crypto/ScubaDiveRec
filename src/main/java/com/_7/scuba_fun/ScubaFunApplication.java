5b7a0b76c96071095be695db0a5a6c87a677ca76
MSI\ofee0 <ofee04@gmail.com>
2026-04-22 18:15:48 +0800

    INIT

---
+++ src/main/java/com/_7/scuba_fun/ScubaFunApplication.java
@@ -0,0 +1,13 @@
+package com._7.scuba_fun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScubaFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScubaFunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_7/scuba_fun/ScubaFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com._7.scuba_fun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScubaFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
